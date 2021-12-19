9c1d3a255874bb77211663e61bcdc6e01e1c7a3e
jaydenhart <jaydenhartprogrammer@gmail.com>
2021-12-19 19:54:28 +1100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+export class AppComponent implements OnInit {
+  /**
+   * @constructor
+   * @description Injects services
+   */
+  constructor(private titleService: Title) {}
 
-export class AppComponent {
-  
+  /**
+   * @name ngOnInit
+   * @function
+   * @returns {void} void
+   * @description Sets the document title
+   */
+  ngOnInit(): void {
+    this.titleService.setTitle('Jayden Hart - Stake Demo');
+  }
 }
